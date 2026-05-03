cc7df98ecbee6c09ec6956ef9eb0f2dc89f0cbf7
Aya <ayacoders@gmail.com>
2026-05-03 16:13:59 +0800

    feat: added placeholder auth pages and configured routing to use lowercase only

--- CloudCompute/Program.cs
+++ CloudCompute/Program.cs
@@ -3,7 +3,6 @@ using CloudCompute.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
 builder.Services.ConfigureServices(builder.Configuration);
 
 var app = builder.Build();
