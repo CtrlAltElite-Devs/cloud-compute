61543607e47c44cfdc69bf85f159ee42218cc224
Aya <ayacoders@gmail.com>
2026-05-03 15:48:25 +0800

    feat: initial landing page design

---
+++ CloudCompute/Controllers/LandingController.cs
@@ -0,0 +1,27 @@
+using System.Diagnostics;
+using CloudCompute.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudCompute.Controllers
+{
+    public class LandingController : Controller
+    {
+        public LandingController(ILogger<LandingController> logger) { }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
