2b46ef4a92dcd55aea608c20e94871d293451f38
Aya <ayacoders@gmail.com>
2026-05-04 20:05:08 +0800

    feat(data): apply development migrations on startup

--- CloudCompute/Program.cs
+++ CloudCompute/Program.cs
@@ -1,5 +1,6 @@
 using CloudCompute.Data;
 using CloudCompute.Extensions;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,18 +9,21 @@ builder.Services.ConfigureServices(builder.Configuration);
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var seeder = scope.ServiceProvider.GetRequiredService<DevelopmentAdminSeeder>();
-    await seeder.SeedAsync();
-}
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Landing/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    using var scope = app.Services.CreateScope();
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await dbContext.Database.MigrateAsync();
+
+    var seeder = scope.ServiceProvider.GetRequiredService<DevelopmentAdminSeeder>();
+    await seeder.SeedAsync();
 }
 
 app.UseHttpsRedirection();
